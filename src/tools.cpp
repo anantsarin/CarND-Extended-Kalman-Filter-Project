a9322c0b217515668d98d72a17b973c4b3af1ebb
Anant Sarin <anant.sarin@gmail.com>
2020-09-12 19:50:10 -0700

    first commint
    stated working on the project
    commit before first run

--- src/tools.cpp
+++ src/tools.cpp
@@ -1,6 +1,6 @@
 #include "tools.h"
 #include <iostream>
-
+using namespace std;
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
@@ -14,6 +14,29 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   /**
    * TODO: Calculate the RMSE here.
    */
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+
+  // check the validity of the following inputs:
+  //  * the estimation vector size should not be zero
+  //  * the estimation vector size should equal ground truth vector size
+  if (estimations.size() != ground_truth.size() || estimations.size() == 0) {
+    cout << "Invalid estimation or ground_truth data" << endl;
+    return rmse;
+  }
+  //accumulate squared residuals
+  for (unsigned int i=0; i < estimations.size(); ++i) {
+    VectorXd residual = estimations[i] - ground_truth[i];
+    // coefficient-wise multiplication
+    residual = residual.array()*residual.array();
+    rmse += residual;
+  }
+  // calculate the mean
+  rmse = rmse / estimations.size();
+  // calculate the squared root
+  rmse  =  rmse.array().sqrt();
+  // return the result
+  return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -21,4 +44,25 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
    * TODO:
    * Calculate a Jacobian here.
    */
+  MatrixXd Hj(3,4);
+  // recover state parameters
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+
+  // check division by zero
+  float pxy = sqrt((px*px) + (py*py));
+  if (pxy < 0.0001){
+    cout << "unable to divide by zero ";
+    cout << Hj;
+    return Hj;
+  }
+
+  // compute the Jacobian matrix
+  Hj << px/pxy , py/pxy, 0 , 0,
+        (-1*py)/(pxy*pxy), px/(pxy*pxy), 0, 0,
+        (py*(vx*py - vy*px))/(pxy*pxy*pxy), (py*(vy*px - vx*py))/(pxy*pxy*pxy), px/pxy , py/pxy ;
+
+  return Hj;
 }
